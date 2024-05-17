af507ee9367dac4b7ebfab01e9c4aac151f26ee5
Arathi Variar <arathivariar@gmail.com>
2024-05-17 10:19:01 +0000

    Added git ignore file to ignore node modules and package-lock.json while committing

---
+++ book-nook-react/src/App.js
@@ -0,0 +1,62 @@
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import ReviewCreateForm from "./pages/reviews/ReviewCreateForm";
+import ReviewPage from "./pages/reviews/ReviewPage";
+import ReviewsPage from "./pages/reviews/ReviewsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+
+
+
+function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <ReviewsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <ReviewsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <ReviewsPage
+                message="No results found. Adjust the search keyword or like a review."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/reviews/create" render={() => <ReviewCreateForm />} />
+          <Route exact path="/reviews/:id" render={() => <ReviewPage />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
