cc6b47a5393f7e333558e7f1c7a77963f6a30725
Arathi Variar <arathivariar@gmail.com>
2024-05-18 21:08:43 +0000

    Added the infinite scroll for comment component

--- book-nook-react/src/pages/reviews/ReviewPage.js
+++ book-nook-react/src/pages/reviews/ReviewPage.js
@@ -9,8 +9,13 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Review from "./Review";
 import Comment from "../comments/Comment";
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function ReviewPage() {
   const { id } = useParams();
@@ -55,14 +60,20 @@ function ReviewPage() {
           "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
+            <InfiniteScroll
+            children={comments.results.map((comment) => (
               <Comment
                 key={comment.id}
                 {...comment}
                 setReview={setReview}
                 setComments={setComments}
               />
-            ))
+            ))}
+            dataLength={comments.results.length}
+            loader={<Asset spinner />}
+            hasMore={!!comments.next}
+            next={() => fetchMoreData(comments, setComments)}
+          />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
