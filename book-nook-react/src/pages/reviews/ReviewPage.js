17c458da01131ba507cc045ea6de6d68f8f5d896
Arathi Variar <arathivariar@gmail.com>
2024-05-18 14:12:55 +0000

    Created comment form so that logged in users can add comments

--- book-nook-react/src/pages/reviews/ReviewPage.js
+++ book-nook-react/src/pages/reviews/ReviewPage.js
@@ -8,10 +8,16 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Review from "./Review";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function ReviewPage() {
   const { id } = useParams();
   const [review, setReview] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -34,7 +40,19 @@ function ReviewPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Review {...review.results[0]} setReviews={setReview} reviewPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+          <CommentCreateForm
+          profile_id={currentUser.profile_id}
+          profileImage={profile_image}
+          review={id}
+          setReview={setReview}
+          setComments={setComments}
+          />
+          ) : comments.results.length ? (
+          "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
