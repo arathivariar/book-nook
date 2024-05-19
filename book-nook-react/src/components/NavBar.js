04e9304f689cf6f06dd5a6c8a6b4965df05979b6
Arathi Variar <arathivariar@gmail.com>
2024-05-19 21:39:51 +0000

    Refactored imports to minimize bootstrap build to only required library components

--- book-nook-react/src/components/NavBar.js
+++ book-nook-react/src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
-import { NavLink } from "react-router-dom";
-import {
-  useCurrentUser,
-  useSetCurrentUser,
-} from "../contexts/CurrentUserContext";
+import NavLink from "react-router-dom/NavLink";
+import useCurrentUser from "../contexts/CurrentUserContext/useCurrentUser";
+import useSetCurrentUser from "../contexts/CurrentUserContext/useSetCurrentUser";
 import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
