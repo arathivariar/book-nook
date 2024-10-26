caedb43c5969149f215ff963b45e525faf91acb5
Arathi Variar <arathivariar@gmail.com>
2024-10-26 09:18:17 +0000

    Added console log to verify review id for comment edit form

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -6,6 +6,7 @@ import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 
 function CommentEditForm(props) {
+  console.log("Props in CommentEditForm: ", props);
   const { id, content, setShowEditForm, setComments, review_id } = props;
 
   const [formContent, setFormContent] = useState(content);
